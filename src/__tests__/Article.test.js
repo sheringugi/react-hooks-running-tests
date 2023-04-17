859fb8782df3ce9844d8cd6867b1b4d8f9e897a6
sheringugi <sheri.ngugi17@gmail.com>
2023-04-17 13:22:51 +0300

    Completed assignment

--- src/__tests__/Article.test.js
+++ src/__tests__/Article.test.js
@@ -4,8 +4,20 @@ import React from "react";
 
 import Article from "../components/Article";
 
+// test("displays the text 'please pass this test'", () => {
+//   render(<Article />);
+  
+//   screen.debug();
+
+//   expect(screen.queryByText("please pass this test")).toBeInTheDocument();
+// });
+
 test("displays the text 'please pass this test'", () => {
   render(<Article />);
 
-  expect(screen.queryByText("please pass this test")).toBeInTheDocument();
+  const element = screen.queryByText("please pass this test");
+
+  screen.debug(element);
+
+  expect(element).toBeInTheDocument();
 });
--- src/__tests__/Header.test.js
+++ src/__tests__/Header.test.js
@@ -4,8 +4,20 @@ import React from "react";
 
 import Header from "../components/Header";
 
-test("displays the text 'hello from the Header!'", () => {
+// test("displays the text 'hello from the Header!'", () => {
+//   render(<Header />);
+
+//   screen.debug();
+
+//   expect(screen.queryByText("hello from the Header!")).toBeInTheDocument();
+// });
+
+test("displays the text 'please pass this test'", () => {
   render(<Header />);
 
-  expect(screen.queryByText("hello from the Header!")).toBeInTheDocument();
+  const element = screen.queryByText("hello from the Header!");
+
+  screen.debug(element);
+
+  expect(element).toBeInTheDocument();
 });
